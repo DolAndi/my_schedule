70ada95fedc563ea222850bf7302f4ab81d3af64
andriano.toazza <andriano@jungle.ai>
2023-03-28 20:55:21 -0300

    update

--- my_schedule/lib/list_classes.dart
+++ my_schedule/lib/list_classes.dart
@@ -4,14 +4,14 @@ import 'package:my_schedule/model/class.dart';
 import 'package:my_schedule/new_class.dart';
 
 class ListClasses extends StatefulWidget {
-  const ListClasses({super.key});
+  const ListClasses({Key? key}) : super(key: key);
 
   @override
   State<ListClasses> createState() => _ListClassesState();
 }
 
 class _ListClassesState extends State<ListClasses> {
-  var classes = [];
+  List<MyClass> classes = [];
 
   @override
   Widget build(BuildContext context) {
@@ -20,30 +20,37 @@ class _ListClassesState extends State<ListClasses> {
           title: const Text("Minhas aulas"),
           actions: [
             IconButton(
-                onPressed: () {
-                  Future future =
-                      Navigator.of(context).pushNamed(ADD_CLASS_PAGE);
-                  future.then((myClass) {
-                    if (myClass != null) {
-                      setState(() {
-                        classes.add(myClass);
-                      });
-                    }
-                  });
-                },
-                icon: const Icon(Icons.add_circle))
+              onPressed: () {
+                Future future = Navigator.of(context).pushNamed(ADD_CLASS_PAGE);
+                future.then((myClass) {
+                  if (myClass != null) {
+                    setState(() {
+                      classes.add(myClass);
+                    });
+                  }
+                });
+              },
+              icon: const Icon(Icons.add_circle),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  classes.clear();
+                });
+              },
+              child: const Text('Limpar lista'),
+            ),
           ],
         ),
         body: ListView.separated(
-            itemBuilder: (context, index) => buildItem(classes[index]),
-            separatorBuilder: (context, index) => const Divider(height: 1),
-            itemCount: classes.length));
+          itemBuilder: (context, index) => buildItem(classes[index]),
+          separatorBuilder: (context, index) => const Divider(height: 1),
+          itemCount: classes.length,
+        ));
   }
 
   Widget buildItem(MyClass myClass) => ListTile(
-        leading: myClass.like
-            ? const Icon(Icons.favorite)
-            : const Icon(Icons.favorite_outline),
+        leading: myClass.like ? const Icon(Icons.favorite) : const Icon(Icons.favorite_outline),
         title: Text(myClass.name),
         subtitle: Text(myClass.dayOfWeek),
         trailing: Text(myClass.local),
